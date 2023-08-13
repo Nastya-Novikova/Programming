9258f27e958fc3bb4c2e97e652dc2ba1178bbc18
Анастасия Новикова <novnastya2005@gmail.com>
2023-08-13 21:06:36 +0700

    Созданы классы Item и ValueValidator. Добавлены комментарии.

--- ObjectOrientedPractices/ObjectOrientedPractices/Model/Item.cs
+++ ObjectOrientedPractices/ObjectOrientedPractices/Model/Item.cs
@@ -6,7 +6,112 @@ using System.Threading.Tasks;
 
 namespace ObjectOrientedPractices.Model
 {
-    internal class Item
+    /// <summary>
+    ///Хранит данные о товаре.
+    /// </summary>
+    public class Item
     {
+        /// <summary>
+        /// Уникальный номер товара.
+        /// </summary>
+        private readonly int _id;
+
+        /// <summary>
+        /// Название товара.
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// Описание товара.
+        /// </summary>
+        private string _info;
+
+        /// <summary>
+        /// Стоимость товара.
+        /// </summary>
+        private double _cost;
+
+        /// <summary>
+        /// Счетчик созданных товаров.
+        /// </summary>
+        private static int _allItemsCount;
+
+        /// <summary>
+        /// Возвращает уникальный номер товара.
+        /// </summary>
+        public int Id => _id;
+
+        /// <summary>
+        /// Возвращает и задает количество созданных товаров.
+        /// </summary>
+        public static int AllItemsCount
+        {
+            get => _allItemsCount;
+            private set => _allItemsCount = value;
+        }
+
+        /// <summary>
+        /// Возвращает и задает название песни.
+        /// Должно быть от 1 до 200 символов.
+        /// </summary>
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                int min = 1;
+                int max = 200;
+                ValueValidator.AssertStringOnLength(value, min, max);
+                _name = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает описание товара.
+        /// Должно быть от 1 до 1000 символов.
+        /// </summary>
+        public string Info
+        {
+            get => _info;
+            set
+            {
+                int min = 1;
+                int max = 1000;
+                ValueValidator.AssertStringOnLength(value, min, max);
+                _info = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает стоимость товара.
+        /// Должна быть от 0 до 100000.
+        /// </summary>
+        public double Cost
+        {
+            get => _cost;
+            set
+            {
+                int min = 0;
+                int max = 100000;
+                ValueValidator.AssertValueInRange(value, min, max);
+                _cost = value;
+            }
+        }
+
+        /// <summary>
+        /// Создает экземпляр класса <see cref="Item"/>.
+        /// </summary>
+        /// <param name="name">Название. Должно быть от 1 до 200 символов.</param>
+        /// <param name="info">Описание. Должно быть от 1 до 1000 символов.</param>
+        /// <param name="cost">Стоимость. Должна быть от 0 до 100000.</param>
+        /// Класс контролирует уникальность присвоенного id.
+        public Item(string name, string info, double cost)
+        {
+            _id = AllItemsCount + 1;
+            AllItemsCount++;
+            Name = name;
+            Info = info;
+            Cost = cost;
+        }
     }
 }
---
+++ ObjectOrientedPractices/ObjectOrientedPractices/Model/ValueValidator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractices.Model
+{
+    /// <summary>
+    /// Предоставляет методы для валидации полей.
+    /// </summary>
+    public class ValueValidator
+    {
+        /// <summary>
+        /// Проверяет длину строки на принадлежность интервалу.
+        /// </summary>
+        /// <param name="value">Проверяемое значение.</param>
+        /// <param name="minLength">Минимум интервала.</param>
+        /// <param name="maxLength">Максимум интервала.</param>
+        /// <param name="property">Имя свойства или объекта, которое подлежит проверке.</param>
+        /// <exception cref="ArgumentException">Возникает, если проверяемое число не принадлежит интервалу.</exception>
+        public static void AssertStringOnLength(string value, int minLength, int maxLength, [CallerMemberName] string property = "")
+        {
+            if (value.Length < minLength || value.Length > maxLength)
+            {
+                throw new ArgumentException($"Значение в {property} должно быть до {maxLength}");
+            }
+        }
+
+        /// <summary>
+        /// Проверяет вещественное число на принадлежность интервалу.
+        /// </summary>
+        /// <param name="value">Проверяемое значение.</param>
+        /// <param name="min">Минимум интервала.</param>
+        /// <param name="max">Максимум интервала.</param>
+        /// <param name="property">Имя свойства или объекта, которое подлежит проверке.</param>
+        /// <exception cref="ArgumentException">Возникает, если проверяемое число не принадлежит интервалу.</exception>
+        public static void AssertValueInRange(double value, int min, int max, [CallerMemberName] string property = "")
+        {
+            if (value < min || value > max)
+            {
+                throw new ArgumentException($"Значение в {property} должно быть от {min} до {max}");
+            }
+        }
+    }
+}
